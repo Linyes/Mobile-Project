005b3d8e9065233bb69f21d0fef1a023132881b2
Ethan Lai <tommylys@gmail.com>
2013-04-25 16:47:44 -0400

    Initial Commit

---
+++ Mobile Project/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Mobile Project
+//
+//  Created by Linyes on 4/25/13.
+//  Copyright (c) 2013 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Mobile Project/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mobile Project
+//
+//  Created by Linyes on 4/25/13.
+//  Copyright (c) 2013 Columbia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
